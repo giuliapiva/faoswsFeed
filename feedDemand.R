1c977c9b36bf593cde27f251b8dd81592b50bad3
sebastian.campbell@fao.org <sebastian.campbell@fao.org>
2015-10-06 12:22:46 +0200

    Integrated feedDemand with SWS

--- feedDemand.R
+++ feedDemand.R
@@ -1,3 +1,5 @@
+# Calculates protein and energy units 
+
 library(faosws)
 library(faoswsUtil)
 library(data.table)
@@ -5,56 +7,86 @@ library(reshape2)
 
 
 #setwd("T:/Team_working_folder/A/Total-Feed-Model/Programming/Programs/functions")
-#source("functions/sws_query.r")
+source("functions/sws_query.r")
 
-GetTestEnvironment("https://hqlprswsas1.hq.un.fao.org:8181/sws", "eca8911c-6d70-4b82-9933-6af56d628c92")
+
+if(CheckDebug()){
+  SetClientFiles("~/certificates/production")
+}
+
+GetTestEnvironment("https://hqlprswsas1.hq.un.fao.org:8181/sws", "ebdda55c-21a4-4bdd-9d0c-5098cec843f7")
 
 
 ## retrieve data on animal numbers from old sws using FCL codes (to be converted into new sws and CPC coding)
-animalHeads = as.data.table(sws_query(area=1:299, item=c(866, 946, 976, 1016, 1034, 1057, 1068, 1072, 1079, 1096,
-                                            1107, 1110, 1126, 1140 ), ele=11, year=1990:2012, value.names=F))
-#key <- 
-animalHeads <- 
+# animalHeads = as.data.table(sws_query(area=1:299, item=c(866, 946, 976, 1016, 1034, 1057, 1068, 1072, 1079, 1096,
+#                                             1107, 1110, 1126, 1140 ), ele=11, year=1990:2012, value.names=F,
+#                                       class.path = 'functions/ojdbc14.jar'))
 
-animalHeads$ele = NULL
-animalHeads$flag = NULL
-setnames(animalHeads, c('geographicAreaFCL', "measuredItemFCL", "timePointYears", "animalHeads"))
+## FS and FCL code are on FAOStat 1 > SUA Working
+
+## PROBLEMATIC CODES
+# 22 (Aruba) is mapped to 532, should be 533
+
+key = DatasetKey(domain="agriculture", dataset="agriculture",
+                  dimensions=list(
+                    Dimension(name="geographicAreaM49", keys=na.omit(fs2m49(as.character((1:299)[-22])))),
+                    Dimension(name="measuredItemCPC", keys=na.omit(fcl2cpc(sprintf("%04d", c(866, 946, 976, 1016, 1034, 1057, 1068, 1072, 1079, 1096,
+                                                                                             1107, 1110, 1126, 1140 ))))),
+                    Dimension(name="measuredElement", keys=c("5111", "5112")),
+                    Dimension(name="timePointYears", keys=as.character(1990:2012))
+                    
+                    )
+                 )
+animalHeads = GetData(key)
+
+#animalHeads$ele = NULL
+#animalHeads$flag = NULL
+animalHeads[, `:=`(measuredElement=NULL, flagObservationStatus=NULL, flagMethod=NULL)]
+setnames(animalHeads, c('geographicAreaM49', "measuredItemCPC", "timePointYears", "animalHeads"))
 
 # Poultry and Rabbits are expressed in '000 heads, hence convert into heads
-animalHeads[, animalHeads := ifelse( measuredItemFCL %in% c(1057, 1068, 1072, 1079, 1140), 
+animalHeads[, animalHeads := ifelse( measuredItemCPC %in% fcl2cpc(as.character(c(1057, 1068, 1072, 1079, 1140))), 
                                      animalHeads * 1000, 
                                      animalHeads)]
 
 ## animal units
-setwd("T:/Team_working_folder/A/Total-Feed-Model/Programming/Data/trans")
-animalUnit = as.data.table(read.csv('animal_unit_6-12.csv')) 
+#setwd("T:/Team_working_folder/A/Total-Feed-Model/Programming/Data/trans")
+animalUnit = as.data.table(read.csv('../Data/trans/animal_unit_6-12.csv'))
 animalUnit = animalUnit[,.(Area.Code, Item.Code, Year, Energy.Factor, Protein.Factor)]
-setnames(animalUnit, c("geographicAreaFCL", "measuredItemFCL", "timePointYears", 
+setnames(animalUnit, c("geographicAreaM49", "measuredItemCPC", "timePointYears", 
                         "energyFactor", "proteinFactor"))
+
+#convert types for merging
+animalUnit[,`:=`(geographicAreaM49=as.character(geographicAreaM49), 
+              measuredItemCPC=as.character(measuredItemCPC), 
+              timePointYears=as.character(timePointYears))]
 
 
 ## Link animal types with intensity groups
-animalFCLGroup = data.table( measuredItemFCL = sort(unique(animalHeads$measuredItemFCL)),
+animalCPCGroup = data.table(measuredItemCPC = sort(unique(animalHeads$measuredItemCPC)),
                                  animalGroup = c(1, 5, 2, 2, 3, 4, 4, 4, 4, 7, 7, 7, 6, 8))                         
 ## intensity factors
 
-intensityFactor  = as.data.table(read.csv("IR-estimated_6-15.csv"))
+intensityFactor  = as.data.table(read.csv("../Data/trans/IR-estimated_6-15.csv"))
 intensityFactor  =  intensityFactor[, .(AREA, AnimGroup, Year, IR)]
 
-setnames(intensityFactor, c("geographicAreaFCL", "animalGroup",'timePointYears', 'intensity' ))
+setnames(intensityFactor, c("geographicAreaM49", "animalGroup",'timePointYears', 'intensity' ))
+#convert types for merging
+intensityFactor[,`:=`(geographicAreaM49=as.character(geographicAreaM49), 
+                      timePointYears=as.character(timePointYears))]
 
 
 # compile Total Feed Demand in Energy and Protein
 
 headUnit = merge(animalHeads, animalUnit, 
-                  by = c("geographicAreaFCL", "measuredItemFCL", "timePointYears"),
+                  by = c("geographicAreaM49", "measuredItemCPC", "timePointYears"),
                   all.x=T, allow.cartesian=T) 
 
-headUnitGroup = merge(headUnit, animalFCLGroup, by = "measuredItemFCL", all.x=T)
+headUnitGroup = merge(headUnit, animalCPCGroup, by = "measuredItemCPC", all.x=T)
 
 
 livestockDemandData = merge(headUnitGroup,intensityFactor, 
-                          by = c("geographicAreaFCL", "animalGroup", "timePointYears"),
+                          by = c("geographicAreaM49", "animalGroup", "timePointYears"),
                           all.x=T) 
 livestockDemandData[is.na(livestockDemandData)] <- 0
 
@@ -64,27 +96,31 @@ livestockDemandData[, energyDemand := animalHeads * energyFactor * intensity * 3
 livestockDemandData[, proteinDemand := animalHeads * proteinFactor * intensity * 0.319]
 
 livestockEnergyDemand = as.data.table(aggregate(data = livestockDemandData, 
-                                          energyDemand ~ geographicAreaFCL + timePointYears, 
+                                          energyDemand ~ geographicAreaM49 + timePointYears, 
                                           sum))
 livestockProteinDemand = as.data.table(aggregate(data = livestockDemandData, 
-                                                 proteinDemand ~ geographicAreaFCL + timePointYears, 
+                                                 proteinDemand ~ geographicAreaM49 + timePointYears, 
                                                  sum))
 livestockDemand = merge(livestockEnergyDemand, livestockProteinDemand,
-                         by = c("geographicAreaFCL", "timePointYears")) 
+                         by = c("geographicAreaM49", "timePointYears")) 
   
-setnames(livestockDemand, c("geographicAreaFCL", "timePointYears", 
+setnames(livestockDemand, c("geographicAreaM49", "timePointYears", 
                             "livestockEnergyDemand", "livestockProteinDemand"))
 ## add aquaculture
 
-aquaDemand = as.data.table(read.csv("aquademand.csv"))
+aquaDemand = as.data.table(read.csv("../Data/trans/aquademand.csv"))
 
 aquaDemand = aquaDemand[, .(area.code, year, energy, protein)]
 
-setnames(aquaDemand, c("geographicAreaFCL", "timePointYears", 
+setnames(aquaDemand, c("geographicAreaM49", "timePointYears", 
                     "aquaEnergyDemand", "aquaProteinDemand" ))
 
+aquaDemand[,`:=`(geographicAreaM49=as.character(geographicAreaM49), 
+                      timePointYears=as.character(timePointYears))]
+
+
 feedDemandData = merge(livestockDemand, aquaDemand, 
-                       by = c("geographicAreaFCL", "timePointYears"),
+                       by = c("geographicAreaM49", "timePointYears"),
                        all=T)
 
 feedDemandData[is.na(feedDemandData)] <- 0
@@ -95,4 +131,5 @@ feedDemandData[is.na(feedDemandData)] <- 0
 feedDemandData[, energyDemand := livestockEnergyDemand + aquaEnergyDemand]
 feedDemandData[, proteinDemand := livestockProteinDemand + aquaProteinDemand]
 
-feedDemand = feedDemandData[, .(geographicAreaFCL, timePointYears, energyDemand, proteinDemand)]
+feedDemand = feedDemandData[, .(geographicAreaM49, timePointYears, energyDemand, proteinDemand)]
+
