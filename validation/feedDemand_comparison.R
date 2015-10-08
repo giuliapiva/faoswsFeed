a293c6bc009e4bcf22636e4933078caa6fa87b5b
Sebastian Campbell <sebastian.campbell@fao.org>
2015-10-08 15:21:41 +0200

    Fixed duplication in validation

--- validation/feedDemand_comparison.R
+++ validation/feedDemand_comparison.R
@@ -1,7 +1,10 @@
+library(faosws)
+library(data.table)
 library(faoswsUtil)
 
 GetTestEnvironment("https://hqlprswsas1.hq.un.fao.org:8181/sws", "ebdda55c-21a4-4bdd-9d0c-5098cec843f7")
 
+#Read in results from old and new system
 oldfile <- data.table(read.csv("oldfeedDemand.csv"))
 newfile <- data.table(read.csv("newfeedDemand.csv"))
 
@@ -14,14 +17,23 @@ setnames(oldfile, c("energyDemand", "proteinDemand"), c("oldEnergy", "oldProtein
 setkey(oldfile, geographicAreaFCL, timePointYears)
 
 mergetest <- newfile[oldfile]
+mergetest <- mergetest[,.(geographicAreaM49, timePointYears, oldEnergy, newEnergy, oldProtein, newProtein)]
+
+keytranslations <- unique(mergetest$geographicAreaM49)
+countrycodes <- GetCodeList("agriculture", "agriculture", "geographicAreaM49", keytranslations[!keytranslations %in% "532"])[,.(code, description)]
+setkey(countrycodes, code)
+mergetest <- countrycodes[mergetest]
+
 
 oldmerge <- mergetest[, .(oldEnergy, oldProtein)]
 newmerge <- mergetest[,.(newEnergy, newProtein)]
 
 #which rows are different?
-diffrows <- which(newmerge != oldmerge, arr.ind=T)[,1]
+diffrows <- unique(which(newmerge != oldmerge, arr.ind=T)[,1])
 #which rows are very different?
-bigdiffs <-which(abs(oldmerge - newmerge) > (0.01 * oldmerge), arr.ind=T)[,1]
+bigdiffs <-unique(which(abs(oldmerge - newmerge) > (0.01 * oldmerge), arr.ind=T)[,1])
 
 mergetest[diffrows,]
-mergetest[bigdiffs,]
+bigdiffs <- mergetest[bigdiffs,]
+
+write.csv(bigdiffs, "bigdiffs.csv", row.names = F)
