a589253a030713291287f84bfbde0f8dcd4dfd85
Sebastian Campbell <sebastian.campbell@fao.org>
2015-10-14 14:44:32 +0200

    Added validation scripts for code table, getting values from functions and finding differences in animal functions

---
+++ validation/get_animal_vars.R
@@ -0,0 +1,27 @@
+library(data.table)
+files <- list.files("functions", pattern="_factor", full.names = TRUE)
+#file <-"functions/buffalo_energy_factor.r"
+
+animal_list <- vector(length(files), mode="list")
+names(animal_list) <- gsub("^functions/|\\.[rR]$", "", files)
+
+for(file in files){
+
+text <- getParseData(parse(file, keep.source=TRUE))
+row1 <- which(text$text == "vars")[1]-1
+if(is.na(row1)) next
+id1 <- text[row1,"id"]
+parent1 <- text[row1,"parent"]
+match_parents <- text[text$parent %in% parent1 ,]
+rowid <- which(match_parents$id==id1)
+linestoextract <- match_parents[c(rowid, rowid+1),]$line1
+
+vartext <-scan(file, what='character', quote="*", sep="*", skip=(linestoextract[1] - 1), nlines=diff(linestoextract), strip.white=TRUE)
+eval(parse(text=paste(vartext, collapse="")))
+
+ele <- sapply(vars, `[[`, 1)
+item <- sapply(vars, `[[`, 2)
+
+animal_list[[names(animal_list)[sapply(names(animal_list), grepl,file)]]] <- list(ele=ele, item=item)
+
+}
