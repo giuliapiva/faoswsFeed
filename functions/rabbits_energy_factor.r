cc2b85f16aee7837625ec8bb14e0e443fa969a4d
Sebastian Campbell <sebastian.campbell@fao.org>
2015-10-14 16:33:54 +0200

    Added rabbit functions to repo (Thanks Bernhard)

---
+++ functions/rabbits_energy_factor.r
@@ -0,0 +1,18 @@
+source('T:/Onno/Feed-Model/Expert-Approach/Programming/Programs/functions/sws_query_2.r')
+
+rabbits_energy_factor <- function(area, year) {
+
+  
+  vars <- list(heads = c(11, 1140), carcass = c(41, 1141))
+  
+  
+  data <- sws_query(area = area, year = year, 
+                    pairs = vars)
+  
+  within(data, {
+    Carcass.Wt <- Carcass.Wt / 1000
+    liveweight <- Carcass.Wt / 10 / 0.55
+    energy <- (200 * 0.0041868 * liveweight * 365) / 35600
+  })
+  
+}
---
+++ functions/rabbits_protein_factor.r
@@ -0,0 +1,18 @@
+source('T:/Onno/Feed-Model/Expert-Approach/Programming/Programs/functions/sws_query_2.r')
+
+horses_protein_factor <- function(area, year) {
+
+  
+  vars <- list(heads = c(11, 1096), carcass = c(41, 1141))
+  
+  
+  data <- sws_query(area = area, year = year, 
+                    pairs = vars)
+  
+  within(data, {
+    Carcass.Wt <- Carcass.Wt / 1000
+    liveweight <- Carcass.Wt / 10 / 0.79
+    protein <- (8.4 * liveweight) / 874.1886
+  })
+  
+}
