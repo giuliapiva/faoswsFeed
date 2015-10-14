d526c10a1d946be613ad50bd86343ea5c0be1af0
Bernhard Dalheimer <bernhard.dalheimer@hotmail.com>
2015-10-14 17:33:54 +0200

    Update camels_energy_factor.r

--- functions/camels_energy_factor.r
+++ functions/camels_energy_factor.r
@@ -1,23 +1,17 @@
-camel_energy_factor <- function(area, year) {
-    
+camels_energy_factor <- function(area, year) {
 
   
-  vars <- list(heads = c(11, 1126), carcass = c(41, 1127), milk = c(51, 1130))
-  
-
+  vars <- list(heads = c(11, 1126), carcass = c(41, 1127))
   
   
   data <- sws_query(area = area, year = year, 
                     pairs = vars)
   
   within(data, {
-    Carcass.Wt <- Carcass.Wt / 10
-    if(!exists("Production")) 
-    {Production <- 0}
-    Production[is.na(Production)] <- 0
-    milkpercamel <- Production * 1000 / Stocks
-    energy <- (365 * (1.8 + 0.1 * Carcass.Wt * 2) + 4.6 * milkpercamel) / 35600
+    
+    liveweight <- Carcass.Wt / 10 / 0.62
+    metabolicweight <- liveweight^0.75
+    protein <- ((0.12 * metabolicweight * 4.184) *365) / 35600
   })
-  
   
 }
