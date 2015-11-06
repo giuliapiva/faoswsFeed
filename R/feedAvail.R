58abbe1afded220696972f704ab992b0b197a383
BDalheimer <bernhard.dalheimer@hotmail.com>
2015-11-06 13:28:37 +0100

    substituted <- for =

--- R/feedAvail.R
+++ R/feedAvail.R
@@ -4,14 +4,14 @@
 # usage of single summands depends on user input. Data is downloaded from FAOSTAT sws
 
 
-feedAvail <- function(area, year, feeditem, vars = c("food", "processed")) {
+feedAvail = function(area, year, feeditem, vars = c("food", "processed")) {
     
-  usedvars <- c("production", "feed", "imports", "exports", vars)
+  usedvars = c("production", "feed", "imports", "exports", vars)
   
-  feedCodeTable <- data.frame(measuredElement = c("5510", "5525", "5015", "5520", "5023", "5153", "5141", "5600", "5900"),
+  feedCodeTable = data.frame(measuredElement = c("5510", "5525", "5015", "5520", "5023", "5153", "5141", "5600", "5900"),
                               variable = c("production", "seed", "waste", "feed", "processed", "other", "food", "imports", "exports"))
   
-  feedCodeTable <- feedCodeTable[variable %in% usedvars]
+  feedCodeTable = feedCodeTable[variable %in% usedvars]
   
   productionKey = DatasetKey(domain = "agriculture", dataset = "agriculture",
                        dimensions = list(
@@ -35,29 +35,29 @@ feedAvail <- function(area, year, feeditem, vars = c("food", "processed")) {
                         sessionId =  slot(swsContext.datasets[[1]], "sessionId")
                         )
   
-  tradeData <- GetData(tradeKey)#, flags=FALSE)
+  tradeData = GetData(tradeKey)#, flags=FALSE)
   setnames(tradeData, "measuredElementTrade", "measuredElement")
   
-  allData <- rbind(productionData, tradeData)
+  allData = rbind(productionData, tradeData)
 
-allData <-  merge(allData, feedCodeTable, all.y = TRUE, by="measuredElement")
+allData =  merge(allData, feedCodeTable, all.y = TRUE, by="measuredElement")
 
-avail <- dcast.data.table(allData, geographicAreaM49 + measuredItemCPC + timePointYears ~ variable, value.var = "Value")
+avail = dcast.data.table(allData, geographicAreaM49 + measuredItemCPC + timePointYears ~ variable, value.var = "Value")
 #### Get rid of NAs
-    avail[is.na(avail)] <- 0
+    avail[is.na(avail)] = 0
 
 ### 1.3 Calculate availability
 avail[, feedAvailability := production + imports - exports - 
                         processed - food - seed - other - waste]
-avail <- avail[,]
+avail = avail[,]
 
-#checkfeed <- avail[, .(checkfeed = sum(feed)), by = .(geographicAreaM49, measuredItemCPC)]
+#checkfeed = avail[, .(checkfeed = sum(feed)), by = .(geographicAreaM49, measuredItemCPC)]
 
 #avail[avail <= 0 , avail := feed]
 
 #If official figure exists, use that instead
-#Avail <- within(Avail, { #Avail <- ifelse(Avail <= 0, feed, Avail)
-#                         Avail <- ifelse(flag == " ", feed, Avail)})
+#Avail = within(Avail, { #Avail = ifelse(Avail <= 0, feed, Avail)
+#                         Avail = ifelse(flag == " ", feed, Avail)})
 
 avail[, .(geographicAreaM49, measuredItemCPC, timePointYears, feed, feedAvailability)]
 
