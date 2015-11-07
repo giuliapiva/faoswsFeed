5258f46f16597b27fca395fcd67b12c9a0a21b1e
Sebastian Campbell <sebastian.campbell@fao.org>
2015-11-07 13:58:34 +0100

    Adjusted feedAvail to take measuredItemCPC as argument and documented. It now doesn't return flags but subsets by them.

--- R/feedAvail.R
+++ R/feedAvail.R
@@ -1,11 +1,17 @@
+#' Calculate Feed Availability
+#' 
+#' This function provides feed availabilities. Using the equation:
+#' Production - Exports + Imports + Stockvariation = Food + Seed + Other Uses + Waste + Processed
+#' usage of single summands depends on user input. Data is downloaded from FAOSTAT sws
+#' 
+#' @param vars character. Variables that should be included along with production, feed, imports and exports
+#' @param measuredItem character. Vector of CPC values by which the results should be subsetted
+#' @param flags character. If not NULL, subset keep only data with these flags
+#' @param negate character. When flags is not NULL, keep only data without these flags
+#' 
+#' @export
 
-# This function provides feed availabilities. Using the equation:
-# Production - Exports + Imports + Stockvariation = Food + Seed + Other Uses + Waste + Processed
-# usage of single summands depends on user input. Data is downloaded from FAOSTAT sws
-
-
-
-feedAvail = function(vars = c("food", "processed"), flags = FALSE) {
+feedAvail = function(vars = c("food", "processed"), measuredItem = feedNutrients$measuredItemCPC, flags = NULL, negate = FALSE) {
 
   usedvars = c("production", "feed", "imports", "exports", vars)
   
@@ -16,7 +22,7 @@ feedAvail = function(vars = c("food", "processed"), flags = FALSE) {
   productionKey = DatasetKey(domain = "agriculture", dataset = "agriculture",
                        dimensions = list(
                          Dimension(name = "geographicAreaM49", keys = getQueryKey("geographicAreaM49")),
-                         Dimension(name = "measuredItemCPC", keys = feedNutrients$measuredItemCPC),
+                         Dimension(name = "measuredItemCPC", keys = measuredItem),
                          Dimension(name = "measuredElement", keys = 
                                      feedCodeTable[variable %in% usedvars & dataset == "production", measuredElement]),
                          Dimension(name = "timePointYears", keys = getQueryKey("timePointYears"))
@@ -24,16 +30,16 @@ feedAvail = function(vars = c("food", "processed"), flags = FALSE) {
                        sessionId =  slot(swsContext.datasets[[1]], "sessionId")
                    )
   
-  productionData = GetData(productionKey, flags = flags)
+  productionData = GetData(productionKey, flags = !is.null(flags))
   setkey(productionData, geographicAreaM49, measuredItemCPC, measuredElement, timePointYears)
   
   #Remove this column so the rbind goes ahead
-  if (flags) {productionData[, flagMethod := NULL]}
+  if (!is.null(flags)) {productionData[, flagMethod := NULL]}
   
   tradeKey = DatasetKey(domain = "trade", dataset = "total_trade_CPC",
                         dimensions = list(
                           Dimension(name = "geographicAreaM49", keys = getQueryKey("geographicAreaM49")),
-                          Dimension(name = "measuredItemCPC", keys = feedNutrients$measuredItemCPC),
+                          Dimension(name = "measuredItemCPC", keys = measuredItem),
                           Dimension(name = "measuredElementTrade", keys = 
                                       feedCodeTable[variable %in% usedvars & dataset == "trade", measuredElement]),
                           Dimension(name = "timePointYears", keys = getQueryKey("timePointYears"))
@@ -42,23 +48,29 @@ feedAvail = function(vars = c("food", "processed"), flags = FALSE) {
                         )
   
 
-  tradeData = GetData(tradeKey, flags = flags)
+  tradeData = GetData(tradeKey, flags = !is.null(flags))
   
   #Trade has different names
   setnames(tradeData, "measuredElementTrade", "measuredElement")
-  if (flags) {setnames(tradeData, "flagTrade", "flagObservationStatus")}
+  if (!is.null(flags)) {setnames(tradeData, "flagTrade", "flagObservationStatus")}
   setkey(tradeData, geographicAreaM49, measuredItemCPC, measuredElement, timePointYears)
   
   #Stick all the data together
   allData = rbind(productionData, tradeData)
+  
+  #If a flag is specified, subset by it
+  if (!is.null(flags)) {
+    if(negate){
+      allData <- allData[!(flagObservationStatus %in% flags),]
+    } else {
+    allData <- allData[flagObservationStatus %in% flags,]
+    }
+    allData[, flagObservationStatus := NULL]
+    }
 
 allData =  merge(allData, feedCodeTable, all.y = TRUE, by = "measuredElement")
 
-terms = c("geographicAreaM49", "measuredItemCPC", "timePointYears")
-if(flags){terms = c(terms, "flagObservationStatus")}
-form = as.formula(paste(paste(terms, collapse = " + "), "~", "variable"))
-
-avail = dcast.data.table(allData, form, value.var = "Value")
+avail = dcast.data.table(allData, geographicAreaM49 + measuredItemCPC + timePointYears ~ variable, value.var = "Value")
 
 #### Get rid of NAs
 avail = avail[!apply(avail, 1, function(x) all(is.na(x))),]
@@ -74,7 +86,10 @@ avail[feedAvailability <= 0 , feedAvailability := feed]
 #Avail = within(Avail, { #Avail = ifelse(Avail <= 0, feed, Avail)
 #                         Avail = ifelse(flag == " ", feed, Avail)})
 
-avail[, c("geographicAreaM49", "measuredItemCPC", "timePointYears", if(flags) "flagObservationStatus" else NULL, "feed", "feedAvailability"), with = FALSE]
+avail <- avail[, .(geographicAreaM49, measuredItemCPC, timePointYears, feed, feedAvailability)]
+setkey(avail, geographicAreaM49, measuredItemCPC, timePointYears)
+
+avail
 
 }
 
