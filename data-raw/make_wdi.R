ab6127f3e938684dc827c41e1504f390d7220b73
BDalheimer <bernhard.dalheimer@hotmail.com>
2016-03-11 14:01:05 +0100

    Restricted time frame, disaggregated Middle Income Group, Included only complete time series

--- data-raw/make_wdi.R
+++ data-raw/make_wdi.R
@@ -18,15 +18,20 @@ if (CheckDebug()) {
 }
 
 ## Agricultural productivity data ##
-rawWdi = data.table(read.csv("data-raw/IR_factor/wdi-productivity_6-12.csv", stringsAsFactors = FALSE))
+#rawWdi = data.table(read.csv("data-raw/IR_factor/wdi-productivity_6-12.csv", stringsAsFactors = FALSE))
+# updated File:
+rawWdi = data.table(read.csv("data-raw/IR_factor/wdiLaborProductivityUpdated.csv", stringsAsFactors = FALSE))
 #Remove useless metadata at bottom of csv by keeping rows with country names
 rawWdi = rawWdi[Country.Name != "",]
 
+# Correction for updated file
+setnames(rawWdi, c("Indicator.Name", "Indicator.Code"), c("Series.Name", "Series.Code"))
 #Make years all one column
 wdi = melt(rawWdi, c("Country.Name", "Country.Code", "Series.Name", "Series.Code"), 
             variable.name = "timePointYears", value.name = "productivity")
 #Remove 'y' prefix
-wdi[, timePointYears := sub("y", "", timePointYears)]
+#wdi[, timePointYears := sub("y", "", timePointYears)]
+wdi[, timePointYears := sub("X", "", timePointYears)]
 #Change names to M49
 wdi[, geographicAreaM49 := as.character(countrycode(Country.Code, "wb", "iso3n"))]
 #Remove any countries that aren't converted (probably Kosovo and Channel Islands)
@@ -34,14 +39,19 @@ wdi = wdi[!is.na(geographicAreaM49),]
 # manually correct countrycode mismatch: China code M49 is 1248
 wdi$geographicAreaM49[wdi$geographicAreaM49 == "156"] = "1248"
 
+startYear = "1990" 
+endYear = max(wdi[, timePointYears])
+wdi = wdi[timePointYears %in% as.character(startYear:endYear)]
 ## Imputation of missing data in wdi
 ## !! PROBLEM !! - countries that don't exist anymore. Former Sudan? Former Soviet Countries?
 ## -- We're going to have to 
 incomeClasses <- data.table(read.csv("data-raw/IR_factor/class.csv"))
 # keep only income groups
-incomeClasses <- incomeClasses[GroupName %in% c("High income", "Low income", "Middle income"),]
+#incomeClasses <- incomeClasses[GroupName %in% c("High income", "Low income", "Middle income"),]
+incomeClasses <- incomeClasses[GroupCode %in% c("HIC", "LIC", "UMC", "LMC"),]
 
 incomeClasses[,geographicAreaM49 := as.character(countrycode(CountryCode, "wb", "iso3n"))]
+setnames(incomeClasses, "GroupCode", "GroupName")
 incomeClasses <- incomeClasses[,.(geographicAreaM49, CountryName, GroupName)]
 
 # Sudan has the wrong name (it should be former Sudan)
@@ -59,8 +69,11 @@ defunctCountries <- data.table(geographicAreaM49 = c("729", "890", "402", "810",
                                                "USSR(-1991)", "Serbia-Monte(1992-2005)", "Gaza Strip(-1995)", 
                                                "Ethiopia PDR(-1992)", "Czechoslovak(-1992)", "Bel-Lux(-1999)"
                                ),
-                               GroupName = c("Middle income", "Middle income", "Middle income", 
-                                             "High income", "Middle income", "Middle income",
+#                              GroupName = c("Middle income", "Middle income", "Middle income", 
+#                                             "High income", "Middle income", "Middle income",
+#                                             "Low income", "High income", "High income"))
+                              GroupName = c("Lower middle income", "Upper middle income", "Upper middle income", 
+                                             "High income", "Upper middle income", "Upper middle income",
                                              "Low income", "High income", "High income"))
 
 incomeClasses <- rbind(incomeClasses, defunctCountries)
@@ -82,8 +95,10 @@ islandCountries[,geographicAreaM49 := fs2m49(as.character(geographicAreaM49))]
 #British Virgin Islands, Anguilla
 training <- wdi_withClasses[!islandCountries, on="geographicAreaM49",]
 
+trainingFullTS = training[, .SD[(all(!is.na(productivity)))], by="geographicAreaM49"]
+
 # Make model  for each year
-imp_model <- lm(productivity~GroupName*timePointYears, data=training)
+imp_model <- lm(productivity~GroupName*timePointYears, data=trainingFullTS)
 
 wdi_withClasses[,imputed := predict(imp_model, data.frame(GroupName, timePointYears))]
 
@@ -108,24 +123,24 @@ wdi_withClasses[,new_productivity := imputeNA(productivity, imputed), by=geograp
 
 # ### plot with red rectangles for NAs
 # 
-# library(ggplot2)
-# plot_wdi <- copy(wdi_withClasses)
-# plot_wdi[,timePointYears := as.numeric(timePointYears)]
-# 
-# 
-# pdf("imputation_trial_plots.pdf")
-# for(i in sort(unique(plot_wdi[,Country.Name]))){
-# gp <- ggplot(plot_wdi[Country.Name == i], aes(x=timePointYears, y=new_productivity, group=Country.Name))+
-#   #facet_wrap(~Country.Name, scales="free_y")+
-#   labs(title=i, y="Agricultural Productivity (2005 dollars)")
-# if(nrow(plot_wdi[is.na(productivity) & Country.Name == i])> 0){
-#   gp <- gp + geom_rect(data=plot_wdi[is.na(productivity) & Country.Name == i], 
-#             aes(xmin = timePointYears-.51, xmax=timePointYears + .51, ymin=-Inf, ymax=Inf), 
-#             fill="indianred", colour = "#00000000")
-# }
-# print(gp + geom_line())
-# }
-# dev.off()
+library(ggplot2)
+plot_wdi <- copy(wdi_withClasses)
+plot_wdi[,timePointYears := as.numeric(timePointYears)]
+
+
+pdf("imputation_trial_plots.pdf")
+for(i in sort(unique(plot_wdi[,Country.Name]))){
+gp <- ggplot(plot_wdi[Country.Name == i], aes(x=timePointYears, y=new_productivity, group=Country.Name))+
+  #facet_wrap(~Country.Name, scales="free_y")+
+  labs(title=i, y="Agricultural Productivity (2005 dollars)")
+if(nrow(plot_wdi[is.na(productivity) & Country.Name == i])> 0){
+  gp <- gp + geom_rect(data=plot_wdi[is.na(productivity) & Country.Name == i], 
+            aes(xmin = timePointYears-.51, xmax=timePointYears + .51, ymin=-Inf, ymax=Inf), 
+            fill="indianred", colour = "#00000000")
+}
+print(gp + geom_line())
+}
+dev.off()
 ## FINAL PROCESSING
 
 wdi_withClasses[is.na(productivity), productivity := new_productivity]
