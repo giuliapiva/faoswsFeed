ef3c8c7b73cfd1daa79455d393a6388f0281f38d
Sebastian Campbell <sebastian.campbell@fao.org>
2016-05-13 11:02:48 +0200

    Revert "Removed ass and mule equations" Added FS codes and correct CPC codes for their meat
    This reverts commit 351474876b274083448ccd8160b1b15251bc2323.

---
+++ R/mule_energy_factor.r
@@ -0,0 +1,35 @@
+mule_energy_factor <- function() {
+
+  year <- getQueryKey("timePointYears")
+  area <- getQueryKey("geographicAreaM49")
+  #area <- na.omit(fs2m49(as.character((1:299)[-22])))
+  
+  rawData <-  getProdData(animal = "mule", fun = "energy", area = area, year = year)
+  
+  namedData <- merge(rawData, codeTable[module == "mule" & fun == "energy", .(measuredItemCPC, measuredElement, variable)], 
+                     by = c("measuredElement", "measuredItemCPC"), all.y = TRUE)
+  
+  data <- dcast.data.table(namedData, geographicAreaM49 + timePointYears ~ variable, value.var = "Value")
+  #remove any full NA rows
+  data <- data[!apply(data, 1, function(x) all(is.na(x))),]
+  
+  #If data is empty, return it
+  if (nrow(data) == 0) {
+    data[,energy := numeric(0)]
+    return(data)
+  }
+  
+  # All missing values are to be treated as zero
+  data[is.na(data)] <- 0
+ 
+  
+  within(data, {
+    
+    #No Conversion: Carcass.Wt comes in kg
+    liveweight <- Carcass.Wt / 0.63
+    kleiberconstant <- 0.75
+    metabolicweight <- liveweight ^ kleiberconstant
+    energy <- (0.1548 *  liveweight) * 356 / 35600 
+  })
+  
+}
